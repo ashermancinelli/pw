c4d8bf6020f644f1b7bda5248030251678ea0da5
Asher Mancinelli <asher.mancinelli@pnnl.gov>
2020-10-11 20:47:31 -0700

    working encryption and querying

--- client.py
+++ client.py
@@ -3,23 +3,38 @@
 import os
 import socket
 import click
+from cryptography.fernet import Fernet
+import json
 
 @click.command()
-@click.argument('secret', nargs=1)
 @click.option('--port', default=5555)
 @click.option('--host', default='localhost')
-def main(secret, port, host):
+@click.option('--secret-file', default='secret')
+@click.option('--query', default=None)
+def main(port, host, secret_file, query):
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((host, port))
-    sock.sendall(secret.encode() + b'\n')
-    ans = ''
+
+    secret = open(secret_file, 'rb').read()
+    sock.sendall(secret + b'\n')
+    f = Fernet(secret)
+
+    ans = b''
     while True:
         resp = sock.recv(1)
         if resp == b'':
             break
-        ans += resp.decode()
-    print(ans.strip())
+        ans += resp
+
+    package = f.decrypt(ans).decode()
+    j = json.loads(package)
+    if query is None:
+        print(j)
+    else:
+        for k, v in j.items():
+            if query in k:
+                print(f'{k}: {v}')
 
 if __name__ == '__main__':
     main()
--- serve.py
+++ serve.py
@@ -5,35 +5,41 @@ import socket
 import socketserver
 import click
 import threading
+from cryptography.fernet import Fernet
 
-def pwserver(secret, package):
+def pwserver(secret: bytes, package: bytes):
     class PWServer(socketserver.StreamRequestHandler):
         def handle(self):
-            self.data = self.rfile.readline().strip().decode()
+            self.data = self.rfile.readline().strip()
             if self.data != secret:
                 print('Failure')
                 self.wfile.write('Incorrect password.\n'.encode())
                 raise Exception()
             else:
                 print('Success')
-            print(self.data)
-            self.wfile.write(package.encode() + b'\n')
+            self.wfile.write(package + b'\n')
     return PWServer
 
 
 @click.command()
 @click.option('--port', default=5555)
 @click.option('--host', default='localhost', help='Use `public` for public host')
-def main(port, host):
+@click.option('--secret-file', default='secret')
+@click.option('--package-file', default='package')
+def main(port, host, secret_file, package_file):
 
-    assert os.path.exists('secret') and os.path.exists('package'), \
+    assert os.path.exists(secret_file) and os.path.exists(package_file), \
             "Must have file secret and package present in cwd"
 
-    secret = open('secret', 'r').read().strip()
-    package = open('package', 'r').read().strip()
+    secret: str = open(secret_file, 'rb').read().strip()
+    package: str = open(package_file, 'r').read().strip()
+    f = Fernet(secret)
+    package: bytes = f.encrypt(package.encode())
 
     print('Serving on', host, port)
-    server = socketserver.TCPServer((host if host != 'public' else '', port), pwserver(secret, package))
+    server = socketserver.TCPServer(
+            (host if host != 'public' else '', port),
+            pwserver(secret, package))
     server.serve_forever()
 
 
